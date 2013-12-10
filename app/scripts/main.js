5aebae4a0cd315f2f7f064d18f507e8db9b97905
daniel beeke <daniel.beeke@gmail.com>
2013-12-10 23:41:58 +0100

    rebuild

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,71 +1,24 @@
 (function ($) {
   
   var map = L.map('map', {
-    // fadeAnimation: false
-  }).setView([51.505, -0.09], 6);
-
-  var date = new Date();
-  var milliseconds = date.getMilliseconds(); 
-  var globalCurrentMap = 'countries'
+    attributionControl: false
+  }).setView([51.505, -0.09], 6)
 
   var baseUrl = function (currentMap) {
-    return 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/' + currentMap + '.tm2&mtime=' + milliseconds;
+    var n = new Date().getTime()
+
+    return 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/' + currentMap + '.tm2&mtime=' + n
   }
 
-  var layer = L.tileLayer(baseUrl('countries')).addTo(map);
+  var layer = L.tileLayer(baseUrl('countries')).addTo(map)
+  var highlightLayer = L.tileLayer(baseUrl('countries')).addTo(map)
+  var utfGrid = new L.UtfGrid('http://{s}.tilemill.studiofonkel.nl/utfgrid-getter/{z}/{x}/{y}.grid.json?callback={cb}&id=tmstyle:///home/administrator/countries.tm2')
 
-  var utfGrid = new L.UtfGrid('http://{s}.tilemill.studiofonkel.nl/utfgrid-getter/{z}/{x}/{y}.grid.json?callback={cb}&id=tmstyle:///home/administrator/countries.tm2');
-
-  map.addLayer(utfGrid);
+  map.addLayer(utfGrid)
 
   utfGrid.on('click', function (e) {
-    globalCurrentMap = e.data.iso_a2
-    layer.setUrl(baseUrl('generated/countries-' + globalCurrentMap))
-  });
-
-  new L.Control.GeoSearch({
-      provider: new L.GeoSearch.Provider.Google(),
-      showMarker: false,
-      zoomLevel: 6
-  }).addTo(map);
-
-  $('#map').on('google_geocode_response', function (event, data) {
-    globalCurrentMap = data.address_components.pop().short_name
-    layer.setUrl(baseUrl('generated/countries-' + globalCurrentMap))
+    highlightLayer.setUrl(baseUrl('generated/countries-' + e.data.iso_a2))
   })
 
-  L.TileLayer.prototype.getTileUrl = function (tilePoint) {
-    if (utfGrid._cache[tilePoint.z + '_' + tilePoint.x + '_' + tilePoint.y]) {
-      var cache= utfGrid._cache[tilePoint.z + '_' + tilePoint.x + '_' + tilePoint.y]
-      var found = false
-
-      if (cache.data) {
-        $.each(cache.data, function (index, item) {
-          if(item.iso_a2 == globalCurrentMap) {
-            found = true
-          }
-        })
-
-        if (!found) {
-          mapToUse = 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/countries.tm2&mtime=' + milliseconds
-
-          return L.Util.template(mapToUse, L.extend({
-            s: this._getSubdomain(tilePoint),
-            z: tilePoint.z,
-            x: tilePoint.x,
-            y: tilePoint.y
-          }, this.options));
-        }
-      }
-    }
-
-    return L.Util.template(this._url, L.extend({
-      s: this._getSubdomain(tilePoint),
-      z: tilePoint.z,
-      x: tilePoint.x,
-      y: tilePoint.y
-    }, this.options));
-  }
-
-})(jQuery);
+})(jQuery)
 
