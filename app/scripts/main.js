da326888a36a4dcb4d54f174a97643842894dbde
daniel beeke <daniel.beeke@gmail.com>
2014-01-08 20:27:37 +0100

    changing to mapbox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,22 @@
 (function ($) {
   
-  var map = L.map('map', {
-    attributionControl: false
-  }).setView([51.505, -0.09], 6)
+  var map = L.mapbox.map('map', 'graphius.gfan7dd3')
+    .setView([52, 5], 8);
 
   var baseUrl = function (currentMap) {
     var n = new Date().getTime()
 
-    return 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/' + currentMap + '.tm2&mtime=' + n
+    return 'http://{s}.tilemill.studiofonkel.nl/style/{z}/{x}/{y}.png?id=tmstyle:///home/administrator/styles/country-variants/country-variants-' + currentMap + '.tm2&mtime=' + n
   }
 
   var layer = L.tileLayer('https://www.mapbox.com/v3/graphius.gfan7dd3/{z}/{x}/{y}.png').addTo(map)
   var highlightLayer = L.tileLayer('https://www.mapbox.com/v3/graphius.gfan7dd3/{z}/{x}/{y}.png').addTo(map)
-  var utfGrid = new L.UtfGrid('http://{s}.tilemill.studiofonkel.nl/utfgrid-getter/{z}/{x}/{y}.grid.json?callback={cb}&id=tmstyle:///home/administrator/countries.tm2')
 
+  var utfGrid = new L.UtfGrid('http://{s}.tilemill.studiofonkel.nl/utfgrid-getter/{z}/{x}/{y}.grid.json?callback={cb}&id=tmstyle:///home/administrator/styles/country-variants/country-variants.tm2')
   map.addLayer(utfGrid)
 
   utfGrid.on('click', function (e) {
-    highlightLayer.setUrl(baseUrl('generated/countries-' + e.data.iso_a2))
+    highlightLayer.setUrl(baseUrl(e.data.iso_alpha2))
   })
 
 })(jQuery)
